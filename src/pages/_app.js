ca33ad070b8e824e2707b674a0136a8fb811c481
Md. Sadik Mahmud Adive <siradive137@gmail.com>
2025-11-19 04:41:18 +0600

    feat: add Vercel analytics and speed insights to the application for performance monitoring

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import { ToastProvider } from '../contexts/ToastContext'
 import ToastContainer from '../components/ui/ToastContainer'
 import Head from 'next/head'
 import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -20,6 +22,8 @@ export default function App({ Component, pageProps }) {
           <div className="page-enter">
             <Component {...pageProps} />
           </div>
+          <Analytics />
+          <SpeedInsights />
         </AppLayout>
         <ToastContainer />
       </ToastProvider>
